557342d86d1acd50777cb373d02c3f51e86968e4
nomi181472 <nomansoomro52@gmail.com>
2021-04-13 17:27:21 +0500

    Add project files.

---
+++ ProcessGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace k173652_Q1_
+{
+    class ProcessGenerator
+    {
+        private readonly Timer timer;
+        private readonly int waitTime;
+        public ProcessGenerator()
+        {
+            this.timer = new Timer(1000)
+            {
+                AutoReset = true
+            };
+            this.timer.Elapsed += this.RunProcess;
+        }
+
+        private void RunProcess(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                using (Process myProcess = new Process())
+                {
+                    myProcess.StartInfo.UseShellExecute = false;
+                    // You can start any process, HelloWorld is a do-nothing example.
+                    myProcess.StartInfo.FileName = "sd";
+                    myProcess.StartInfo.CreateNoWindow = true;
+                    myProcess.Start();
+                    // This code assumes the process you are starting will terminate itself.
+                    // Given that is is started without a window so you cannot terminate it
+                    // on the desktop, it must terminate itself or you can do it programmatically
+                    // from this application using the Kill method.
+                }
+            }
+            catch 
+            {
+            }
+
+
+        }
+        public void Start()
+        {
+            this.timer.Start();
+        }
+        public void Stop()
+        {
+            this.timer.Stop();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Topshelf;
+
+namespace k173652_Q1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Environment.ExitCode = (int)Convert.ChangeType(HostFactory.Run
+               (
+                service =>
+                {
+                    service.Service<ProcessGenerator>(pg =>
+                    {
+                        pg.ConstructUsing(processGenerator => new ProcessGenerator());
+                        pg.WhenStarted(processGenerator => processGenerator.Start());
+                        pg.WhenStopped(processGenerator => processGenerator.Stop());
+
+                    });
+                    service.RunAsLocalSystem();
+                    service.SetServiceName("DataFetcherFromPakStockExchange");
+                    service.SetDisplayName(" Data Fetching...");
+                    service.SetDescription("This Service run the question 1 from assignment 1 ");
+
+
+                }
+               ), typeof(TopshelfExitCode));
+
+
+        }
+    }
+}
